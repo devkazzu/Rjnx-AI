358c40af292135b60a44b22f014cd354792b0aa9
devkazzu <kazzumahato@gmail.com>
2026-08-13 09:48:26 +0530

    Add files via upload

--- app/src/main/java/com/rjnx/ai/MainActivity.kt
+++ app/src/main/java/com/rjnx/ai/MainActivity.kt
@@ -167,7 +167,7 @@ class MainActivity : ComponentActivity(), TextToSpeech.OnInitListener {
             command.contains("notes delete") -> clearNotes()
 
             command.contains("youtube") -> {
-                openInstalledAppByName("youtube") || run {
+                if (!openInstalledAppByName("youtube")) {
                     openUrl("https://www.youtube.com")
                     reply("Opening YouTube.")
                 }
